a8cd9ee319bd0e742e04befda7f1359db9c50d0e
최도경_OIS팀 <dokyung.choi@yanolja.com>
2024-06-10 00:44:32 +0900

    node server init

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+// middleware example
+const requestTime = function (
+  req: { requestTime: number },
+  res: any,
+  next: () => void,
+) {
+  req.requestTime = Date.now();
+  next();
+};
+
+app.get("/api/v1/", (req, res) => {
+  res.send("노무현은 살아있다!");
+});
+
+// oracle
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
